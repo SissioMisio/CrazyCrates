70ed540a250c837efaa6ca41d59cb33afb7620a3
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-02 12:04:42 -0400

    Update assemble task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.kotlin.dsl.support.uppercaseFirstChar
-
 plugins {
     `root-plugin`
 }
@@ -14,17 +12,17 @@ tasks {
             jarsDir.mkdirs()
         }
 
-        subprojects.filter { it.name == "paper" || it.name == "fabric" }.forEach { project ->
+        subprojects.filter { it.name == "paper"}.forEach { project ->
             dependsOn(":${project.name}:build")
 
             doLast {
                 runCatching {
-                    val file = File("$jarsDir/${project.name.uppercaseFirstChar().lowercase()}")
+                    val file = File("$jarsDir/${project.name.lowercase()}")
 
                     file.mkdirs()
 
                     copy {
-                        from(project.layout.buildDirectory.file("libs/${rootProject.name}-${project.version}.jar"))
+                        from(project.layout.buildDirectory.file("libs/${rootProject.name}-${rootProject.version}.jar"))
                         into(file)
                     }
                 }.onSuccess {
