682b82efa5194beb64c9be1deec171e82814bb16
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-02 12:06:00 -0400

    Remove extra glass

--- paper/src/main/java/com/badbones69/crazycrates/platform/utils/MiscUtils.java
+++ paper/src/main/java/com/badbones69/crazycrates/platform/utils/MiscUtils.java
@@ -159,7 +159,6 @@ public class MiscUtils {
      *
      * @return -1 or item amount.
      */
-    @SuppressWarnings("SameParameterValue")
     private static int getFirstItem(ItemStack item, boolean getAmount, ItemStack[] inventory) {
         if (item == null) return -1;
 
@@ -218,21 +217,6 @@ public class MiscUtils {
 
     public static ItemBuilder getRandomPaneColor() {
         List<Material> panes = Arrays.asList(
-                Material.LIGHT_BLUE_STAINED_GLASS_PANE,
-                Material.MAGENTA_STAINED_GLASS_PANE,
-                Material.YELLOW_STAINED_GLASS_PANE,
-                Material.PURPLE_STAINED_GLASS_PANE,
-                Material.ORANGE_STAINED_GLASS_PANE,
-                Material.GREEN_STAINED_GLASS_PANE,
-                Material.BROWN_STAINED_GLASS_PANE,
-                Material.BLACK_STAINED_GLASS_PANE,
-                Material.BLUE_STAINED_GLASS_PANE,
-                Material.CYAN_STAINED_GLASS_PANE,
-                Material.GRAY_STAINED_GLASS_PANE,
-                Material.LIME_STAINED_GLASS_PANE,
-                Material.PINK_STAINED_GLASS_PANE,
-                Material.RED_STAINED_GLASS_PANE,
-
                 Material.LIGHT_BLUE_STAINED_GLASS_PANE,
                 Material.MAGENTA_STAINED_GLASS_PANE,
                 Material.YELLOW_STAINED_GLASS_PANE,
