10edd26215bcd0064a000c92131363ca823570d5
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-02 11:30:07 -0400

    Improve server object

--- api/src/main/java/us/crazycrew/crazycrates/platform/Server.java
+++ api/src/main/java/us/crazycrew/crazycrates/platform/Server.java
@@ -1,17 +1,44 @@
 package us.crazycrew.crazycrates.platform;
 
-import us.crazycrew.crazycrates.CrazyCratesProvider;
+import us.crazycrew.crazycrates.CratesProvider;
 import us.crazycrew.crazycrates.platform.config.ConfigManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.util.List;
 import java.util.logging.Logger;
 
-public abstract class Server {
+public class Server {
 
-    public Server(File dataFolder) {
-        ConfigManager.load(dataFolder);
+    private final JavaPlugin plugin;
+    private final File crateFolder;
+    private final File keyFolder;
 
-        CrazyCratesProvider.register(this);
+    public Server(JavaPlugin plugin) {
+        this.plugin = plugin;
+
+        this.keyFolder = new File(this.plugin.getDataFolder(), "keys");
+
+        this.crateFolder = new File(this.plugin.getDataFolder(), "crates");
+    }
+
+    public void enable() {
+        ConfigManager.load(this.plugin.getDataFolder());
+
+        List.of(
+                "CrateExample.yml",
+                "WarCrateExample.yml",
+                "QuadCrateExample.yml",
+                "QuickCrateExample.yml"
+        ).forEach(file -> this.plugin.saveResource(this.crateFolder + "/" + file, false));
+
+        List.of(
+                "CasinoKey.yml",
+                "DiamondKey.yml"
+        ).forEach(file -> this.plugin.saveResource(this.keyFolder + "/" + file, false));
+
+        CratesProvider.register(this);
     }
 
     public void reload() {
@@ -19,22 +46,30 @@ public abstract class Server {
     }
 
     public void disable() {
-        ConfigManager.save();
-
-        CrazyCratesProvider.unregister();
+        CratesProvider.unregister();
     }
 
-    public abstract Logger getLogger();
+    public @NotNull File getFolder() {
+        return this.plugin.getDataFolder();
+    }
 
-    public abstract boolean isLogging();
+    public @NotNull Logger getLogger() {
+        return this.plugin.getLogger();
+    }
 
-    public abstract File getFolder();
+    public @NotNull File getKeyFolder() {
+        return this.keyFolder;
+    }
 
-    public abstract File getKeyFolder();
+    public @NotNull File getCrateFolder() {
+        return this.crateFolder;
+    }
 
-    public abstract File getCrateFolder();
+    public @NotNull File[] getKeyFiles() {
+        return this.keyFolder.listFiles((dir, name) -> name.endsWith(".yml"));
+    }
 
-    public abstract File[] getKeyFiles();
-
-    public abstract File[] getCrateFiles();
+    public @NotNull File[] getCrateFiles() {
+        return this.crateFolder.listFiles((dir, name) -> name.endsWith(".yml"));
+    }
 }
