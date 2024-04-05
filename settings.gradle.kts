fd359e7e90adaaa08e3f88dfef52c1d6e8bd8647
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-05 03:25:44 -0400

    Remove unneeded code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,5 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-pluginManagement {
-    repositories {
-        maven("https://repo.papermc.io/repository/maven-public/")
-
-        gradlePluginPortal()
-        mavenCentral()
-    }
-}
-
 rootProject.name = "CrazyCrates"
 
 include(":api")
