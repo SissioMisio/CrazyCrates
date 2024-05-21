dc7a97e97217d93014823be91a824656977a607f
Ryder Belserion <no-reply@ryderbelserion.com>
2024-05-21 00:04:21 -0400

    Update DecentHologramsSupport.java
    Signed-off-by: Ryder Belserion <no-reply@ryderbelserion.com>

--- paper/src/main/java/com/badbones69/crazycrates/support/holograms/types/DecentHologramsSupport.java
+++ paper/src/main/java/com/badbones69/crazycrates/support/holograms/types/DecentHologramsSupport.java
@@ -29,8 +29,6 @@ public class DecentHologramsSupport extends HologramManager {
 
         Hologram hologram = DHAPI.createHologram(name(), location.clone().add(getVector(crate)));
 
-        DHAPI.addHologramPage(hologram, lines(crateHologram));
-
         crateHologram.getMessages().forEach(line -> DHAPI.addHologramLine(hologram, color(line)));
 
         hologram.setDisplayRange(crateHologram.getRange());
