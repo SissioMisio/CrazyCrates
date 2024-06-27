ff06fd9aacfed647f56fda716a21817ba8757838
SissioMisio <manderioli25@gmail.com>
2024-06-27 21:39:24 +0200

    mezzo hardcode per fare andare i colori nuovi su decentholograms

--- paper/src/main/java/com/badbones69/crazycrates/support/holograms/types/DecentHologramsSupport.java
+++ paper/src/main/java/com/badbones69/crazycrates/support/holograms/types/DecentHologramsSupport.java
@@ -33,10 +33,35 @@ public class DecentHologramsSupport extends HologramManager {
 
         crateHologram.getMessages().forEach(line -> {
             if (line != null) {
-                String coloredLine = color(line);
+                String coloredLine1 = color(line);
 
-                if (coloredLine != null) {
+                if (coloredLine1 != null) {
+                    String coloredLine = coloredLine1
+                            .replace("<GREEN>", "&a")
+                            .replace("<AQUA>", "&b")
+                            .replace("<RED>", "&c")
+                            .replace("<LIGHT_PURPLE>", "&d")
+                            .replace("<YELLOW>", "&e")
+                            .replace("<WHITE>", "&f")
+                            .replace("<BLACK>", "&0")
+                            .replace("<DARK_BLUE>", "&1")
+                            .replace("<DARK_GREEN>", "&2")
+                            .replace("<DARK_AQUA>", "&3")
+                            .replace("<DARK_RED>", "&4")
+                            .replace("<DARK_PURPLE>", "&5")
+                            .replace("<GOLD>", "&6")
+                            .replace("<GRAY>", "&7")
+                            .replace("<DARK_GRAY>", "&8")
+                            .replace("<DARK_BLUE>", "&9")
+                            .replace("<OBFUSCATED>", "&k")
+                            .replace("<BOLD>", "&l")
+                            .replace("<STRIKETHROUGH>", "&m")
+                            .replace("<UNDERLINED>", "&n")
+                            .replace("<ITALIC>", "&o")
+                            .replace("<RESET>", "&r");
+
                     DHAPI.addHologramLine(hologram, coloredLine);
+
                 }
             }
         });
